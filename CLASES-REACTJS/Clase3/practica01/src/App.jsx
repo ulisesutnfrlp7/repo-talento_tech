1f3bbf1f55be092e3d36a67a98d972cd26e48556
Ulises Mateo Bucchino <ulibucchino@gmail.com>
2025-09-15 21:02:32 -0300

    inicio actividades clase 3

--- CLASES-REACTJS/Clase3/practica01/src/App.jsx
+++ CLASES-REACTJS/Clase3/practica01/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
 // import './App.css' // SE SACA YA QUE SE DARÁ ESTILADO CON BOOTSTRAP
 import Header from './components/Header'
 import Navigation from './components/Nav'
 import Main from './components/Main'
 import Gallery from './components/Gallery'
 import Footer from './components/Footer'
+import EquipoTalentoLab from './components/EquipoTalentoLab'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const equipo = [
+    { nombre: 'Silvia', rol: 'Product Owner', imagen: 'https://via.placeholder.com/100' },
+    { nombre: 'Luis', rol: 'Diseñador UX/UI', imagen: 'https://via.placeholder.com/100' },
+    { nombre: 'Matías', rol: 'Desarrollador', imagen: 'https://via.placeholder.com/100' },
+    { nombre: 'Sabrina', rol: 'Desarrolladora', imagen: 'https://via.placeholder.com/100' },
+  ];
+    
 
   return (
     <div>
@@ -17,6 +24,7 @@ function App() {
       <Main/>
       <Gallery/>
       <Footer/>
+      <EquipoTalentoLab equipo={equipo}/>
     </div>
   )
 }
