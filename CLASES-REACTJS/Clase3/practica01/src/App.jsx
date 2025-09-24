4471559cbe2539e441f353eb19134ea83ae75b14
Ulises Mateo Bucchino <ulibucchino@gmail.com>
2025-09-23 23:02:21 -0300

    mando al otro lado la practica

--- CLASES-REACTJS/Clase3/practica01/src/App.jsx
+++ CLASES-REACTJS/Clase3/practica01/src/App.jsx
@@ -4,19 +4,9 @@ import Navigation from './components/Nav'
 import Main from './components/Main'
 import Gallery from './components/Gallery'
 import Footer from './components/Footer'
-import EquipoTalentoLab from './components/EquipoTalentoLab'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-
-  const equipo = [
-    { nombre: 'Silvia', rol: 'Product Owner', imagen: 'https://via.placeholder.com/100' },
-    { nombre: 'Luis', rol: 'Diseñador UX/UI', imagen: 'https://via.placeholder.com/100' },
-    { nombre: 'Matías', rol: 'Desarrollador', imagen: 'https://via.placeholder.com/100' },
-    { nombre: 'Sabrina', rol: 'Desarrolladora', imagen: 'https://via.placeholder.com/100' },
-  ];
-    
-
   return (
     <div>
       <Header/>
@@ -24,7 +14,6 @@ function App() {
       <Main/>
       <Gallery/>
       <Footer/>
-      <EquipoTalentoLab equipo={equipo}/>
     </div>
   )
 }
